a5d5f129b55b7a9413cde78351402da52c1d9938
luzhenting <homeswallow@163.com>
2019-08-27 17:54:15 +0800

    Default Changelist

--- plus_common/src/main/java/cn/pipilu/plus/common/base/entity/BaseEntity.java
+++ plus_common/src/main/java/cn/pipilu/plus/common/base/entity/BaseEntity.java
@@ -3,9 +3,11 @@ package cn.pipilu.plus.common.base.entity;
 import lombok.Data;
 import lombok.ToString;
 
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 @Data
 @ToString
+//@MappedSuperclass
 public class BaseEntity implements Serializable {
     private static final long serialVersionUID = 7931656564364552127L;
     private String id;
